7c7834db78409348e5b7863a7c7858b04b597800
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-05-28 12:25:04 +0200

    Gapotchenko.Shields.Canonical.Snap: refactoring.

--- Catalog/Canonical/Snap/Source/Gapotchenko.Shields.Canonical.Snap.Deployment/Utils/SinglePathResolver.cs
+++ Catalog/Canonical/Snap/Source/Gapotchenko.Shields.Canonical.Snap.Deployment/Utils/SinglePathResolver.cs
@@ -2,7 +2,7 @@
 
 readonly struct SinglePathResolver(string rootPath)
 {
-    public string RootPath { get; } = PathEx.TrimEndingDirectorySeparator(rootPath);
+    public string RootPath { get; } = Path.TrimEndingDirectorySeparator(rootPath);
 
     public string ResolvePath(string? relativePath) => PathUtil.ConstructPath(RootPath, relativePath);
 }
